1d3d83d1778d924964a9662f74a8b10e36f21b6e
liangWenPeng <2653816956@qq.com>
2017-10-31 14:48:50 +0800

    修改数据库地址

--- scrapy_admin/settings.py
+++ scrapy_admin/settings.py
@@ -1,11 +1,11 @@
 
 # mongodb数据库
-MONGODB_URI = 'mongodb://spider:lcworld2211@47.93.119.105:27017'
-MONGODB_DATABASE = 'lcworld'
-MONGO_STATES_COLLECTION = 'spider_state'
+MONGODB_URI = ''
+MONGODB_DATABASE = ''
+MONGO_STATES_COLLECTION = ''
 
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+
 
 # if DEBUG:
 #     MONGODB_URI = 'mongodb://localhost:27017'
